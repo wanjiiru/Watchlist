465ae5ebcab940afc5ac67cb5c7ea898f76114a6
wanjiiru <liz240@live.com>
2018-06-12 21:20:22 +0300

    add wtf forms

---
+++ app/forms.py
@@ -0,0 +1,18 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, TextAreaField,SubmitField
+# from wtforms.validators import Required
+#
+#
+# class ReviewForm(FlaskForm):
+#     title = StringField('Review title', validators=[Required()])
+#     submit = SubmitField('submit')
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+
+class ReviewForm(FlaskForm):
+    title = StringField('Review title', validators=[Required()])
+    review = TextAreaField('Movie review', validators=[Required()])
+    submit = SubmitField('Submit')
