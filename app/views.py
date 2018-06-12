c7bbec1fa6bfd0cb38664b5f345e7810be54f420
wanjiiru <liz240@live.com>
2018-06-12 19:56:16 +0300

    load images

--- app/views.py
+++ app/views.py
@@ -1,20 +1,22 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies, get_movie
+
 
 # Views
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
     '''
     View movie page function that returns the movie details page and its data
     '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
 
-    return render_template('movie.html')
+    return render_template('movie.html', title = title, movie = movie)
 
 
 @app.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
@@ -24,4 +26,5 @@ def index():
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+    return render_template('index.html', title=title, popular=popular_movies, upcoming=upcoming_movie,
+                           now_showing=now_showing_movie)
