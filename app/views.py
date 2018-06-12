705d1e834ac19c7d315a8b1b01f3310a4db147c8
wanjiiru <liz240@live.com>
2018-06-12 21:31:07 +0300

    add review

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from app import app
+from .models import reviews
+from .forms import ReviewForm
+
 from .request import get_movies, get_movie, search_movie
 from flask import render_template, request, redirect, url_for
+
+Review = reviews.Review
 
 
 # Views
@@ -48,3 +53,20 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html', movies=searched_movies)
+
+
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.image,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
